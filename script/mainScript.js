72e741fd12af8ecfbac0104d0e18219dce4ccfea
Cedric Daunke <cedric.daunke@thinksurance.de>
2024-08-13 13:16:32 +0200

    added weather project, changed design of project section

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,5 +1,6 @@
 const githubProfil = "https://github.com/Ceda1998";
 const githubJoin = "https://github.com/Ceda1998/join"
+const githubWeatherApp = "https://github.com/Ceda1998/WeatherApp"
 
 function goToGithub(url) {
     window.open(url, '_blank').focus();
